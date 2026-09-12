4ff17a25bc85712effbef17b589e5ae462576348
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-09-13 00:06:44 +0530

    chore: initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
